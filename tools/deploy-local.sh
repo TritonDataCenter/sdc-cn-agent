d3c0d61b67d3e6ff9be34d4fb13e2f8df6703b38
Orlando Vazquez <ovazquez@gmail.com>
2014-08-12 08:18:57 +0000

    CNAPI-438: use two different kind of messages w/ different periods

--- tools/deploy-local.sh
+++ tools/deploy-local.sh
@@ -2,7 +2,7 @@ set -o xtrace
 DIRNAME=$(cd `dirname $0`/.. && pwd)
 cd $DIRNAME
 
-rsync --recursive --partial -l ./{package.json,scripts,deps,bin,lib,test} /opt/smartdc/agents/lib/node_modules/cn-agent
+rsync --recursive --partial -l ./{package.json,deps,bin,lib,test} /opt/smartdc/agents/lib/node_modules/cn-agent
 
 if [[ -z "$NO_RESTART" ]]; then
     svcadm restart cn-agent
