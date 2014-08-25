edfa0cf9118b9aa725775d12fc026005183fb4b9
Orlando Vazquez <ovazquez@gmail.com>
2014-08-25 21:19:54 +0000

    TOOLS-607 I for one, welcome my new open source license

--- tools/deploy-local.sh
+++ tools/deploy-local.sh
@@ -1,3 +1,13 @@
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2014, Joyent, Inc.
+#
+
 set -o xtrace
 DIRNAME=$(cd `dirname $0`/.. && pwd)
 cd $DIRNAME
