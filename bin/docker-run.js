e7baca0567adc093d892479a52f33d33cee822a1
Orlando Vazquez <ovazquez@gmail.com>
2014-11-10 20:56:12 +0000

    DOCKER-4: Add docker cmd runner, stdio server

---
+++ bin/docker-run.js
@@ -0,0 +1,30 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc.
+ */
+
+var net = require('net');
+var createDockerStdioServer =
+    require('../lib/dockerserver').createDockerStdioServer;
+
+var SERVER_CLOSE_TIMEOUT = 5000;
+
+process.on('message', function (message) {
+    var command = message.command;
+    var uuid = message.uuid;
+
+    var opts = {
+        command: message.command,
+        uuid: message.uuid,
+        timeoutSeconds: 5
+    };
+
+    var server = createDockerStdioServer(opts);
+    process.send({ port: server.address().port });
+});
+
