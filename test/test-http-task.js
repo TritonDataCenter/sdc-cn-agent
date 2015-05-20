facf0dd0c9b5609b4092f3cd28a293e0e97e0de5
Josh Wilsdon <jwilsdon@joyent.com>
2015-05-20 12:27:08 -0700

    AGENT-902 test should connect to correct IP.

--- test/test-http-task.js
+++ test/test-http-task.js
@@ -5,22 +5,58 @@
  */
 
 /*
- * Copyright (c) 2014, Joyent, Inc.
+ * Copyright (c) 2015, Joyent, Inc.
  */
 
 var testCase = require('nodeunit').testCase;
 var restify = require('restify');
 var Logger = require('bunyan');
+var smartdcconfig = require('../lib/smartdc-config');
 
 var PROVISIONER_PORT = 5309;
 var client;
 
+function firstAdminIp(sysinfo) {
+    var interfaces;
+
+    interfaces = sysinfo['Network Interfaces'];
+
+    for (var iface in interfaces) {
+        if (!interfaces.hasOwnProperty(iface)) {
+            continue;
+        }
+
+        var nic = interfaces[iface]['NIC Names'];
+        var isAdmin = nic.indexOf('admin') !== -1;
+        if (isAdmin) {
+            var ip = interfaces[iface].ip4addr;
+            return ip;
+        }
+    }
+
+    throw new Error('No NICs with name "admin" detected.');
+}
+
 function setup(cb) {
-    client = restify.createJsonClient({
-        agent: false,
-        url: 'http://localhost:' + PROVISIONER_PORT
+    smartdcconfig.sysinfo(function (err, sysinfo) {
+        var adminip;
+
+        if (err) {
+            cb(err);
+            return;
+        }
+
+        adminip = firstAdminIp(sysinfo);
+        if (!adminip) {
+            throw new Error('failed to find admin IP');
+        }
+
+        client = restify.createJsonClient({
+            agent: false,
+            url: 'http://' + adminip + ':' + PROVISIONER_PORT
+        });
+        cb();
     });
-    cb();
 }
 
 function teardown(cb) {
@@ -31,8 +67,10 @@ function testExecuteTaskHttp(test) {
     test.expect(3);
     client.get('/tasks', function (err, req, res, tasks) {
         test.ifError(err);
-        test.ok(res, 'got a response');
-        test.equal(res.statusCode, 200, 'GET /tasks returned 200');
+        if (!err) {
+            test.ok(res, 'got a response');
+            test.equal(res.statusCode, 200, 'GET /tasks returned 200');
+        }
         test.done();
     });
 }
