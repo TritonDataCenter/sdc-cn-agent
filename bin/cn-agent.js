1ef274dd4cf39c7e15159e92e44c96723ca47860
Orlando Vazquez <ovazquez@gmail.com>
2016-02-15 00:52:01 -0800

    TOOLS-1331: cn-agent should accept sdc configuration as an parameter at instantiating time

--- bin/cn-agent.js
+++ bin/cn-agent.js
@@ -5,31 +5,37 @@
  */
 
 /*
- * Copyright (c) 2014, Joyent, Inc.
+ * Copyright (c) 2015, Joyent, Inc.
  */
 
-var util = require('util');
-var TaskAgent = require('../lib/task_agent/task_agent');
-var fs = require('fs');
-var path = require('path');
-var dispatch = require('../lib/task_agent/dispatch');
-var createTaskDispatchFn = dispatch.createTaskDispatchFn;
-var createHttpTaskDispatchFn = dispatch.createHttpTaskDispatchFn;
-var os = require('os');
-var exec = require('child_process').exec;
-var tty = require('tty');
-var once = require('once');
 var bunyan = require('bunyan');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var once = require('once');
+var os = require('os');
+var path = require('path');
+var tty = require('tty');
+var util = require('util');
+var vasync = require('vasync');
+var verror = require('verror');
+
 var AgentHttpServer = require('../lib/server');
 var App = require('../lib/app');
+var TaskAgent = require('../lib/task_agent/task_agent');
+var dispatch = require('../lib/task_agent/dispatch');
+var sdcconfig = require('../lib/smartdc-config');
+
+var createHttpTaskDispatchFn = dispatch.createHttpTaskDispatchFn;
+var createTaskDispatchFn = dispatch.createTaskDispatchFn;
 
 main();
 
 function main() {
     var logname = 'cn-agent';
-    var smartdcconfig = require('../lib/smartdc-config');
 
     var log = bunyan.createLogger({ name: logname });
+    var sysinfo;
+    var sdc_config;
 
 
     // The plan is to migrate to using this file as the entire configuration
@@ -47,47 +53,68 @@ function main() {
         agentConfig = { no_rabbit: false };
     }
 
-    if (agentConfig.no_rabbit) {
-        smartdcconfig.sysinfo(function (err, sysinfo) {
-            if (err) {
-                throw err;
-            }
-
-            var ip = firstAdminIp(sysinfo);
-
-            var agentServer = new AgentHttpServer({
-                bindip: ip,
-                log: log,
-                uuid: sysinfo.UUID
-            });
-            agentServer.start();
-
-            var options = {
-                uuid: sysinfo.UUID,
-                log: log,
-                tasklogdir: '/var/log/' + logname + '/logs',
-                logname: logname,
-                taskspath: path.join(__dirname, '..', 'lib/tasks'),
-                agentserver: agentServer
-            };
-
-            var app = new App(options);
-
-            // EXPERIMENTAL
-            if (agentConfig.fluentd_host) {
-                process.env.FLUENTD_HOST = agentConfig.fluentd_host;
-            }
-
-            app.start();
-        });
-
-    } else {
+    if (!agentConfig.no_rabbit) {
         log.warn('"no_rabbit" flag is not true, cn-agent will now sleep');
         // http://nodejs.org/docs/latest/api/all.html#all_settimeout_cb_ms
         // ...The timeout must be in the range of 1-2,147,483,647 inclusive...
         setInterval(function () {}, 2000000000);
     }
+
+
+    vasync.waterfall([
+        function (next) {
+            sdcconfig.sysinfo(function (err, sysinfoObj) {
+                if (err) {
+                    next(new verror.VError(err, 'fetching sysinfo'));
+                    return;
+                }
+
+                sysinfo = sysinfoObj;
+                next();
+            });
+        },
+        function (next) {
+            sdcconfig.sdcConfig(function (error, configObj) {
+                if (error) {
+                    next(new verror.VError(error, 'fetching SDC config'));
+                    return;
+                }
+                sdc_config = configObj;
+                next();
+            });
+        }
+    ],
+    function (e) {
+        var ip = firstAdminIp(sysinfo);
+
+        var agentServer = new AgentHttpServer({
+            bindip: ip,
+            log: log,
+            uuid: sysinfo.UUID
+        });
+        agentServer.start();
+
+        var options = {
+            uuid: sysinfo.UUID,
+            log: log,
+            tasklogdir: '/var/log/' + logname + '/logs',
+            logname: logname,
+            taskspath: path.join(__dirname, '..', 'lib/tasks'),
+            agentserver: agentServer,
+            sdc_config: sdc_config
+        };
+
+        var app = new App(options);
+
+        // EXPERIMENTAL
+        if (agentConfig.fluentd_host) {
+            process.env.FLUENTD_HOST = agentConfig.fluentd_host;
+        }
+
+        app.start();
+    });
 }
+
 
 function firstAdminIp(sysinfo) {
     var interfaces;
