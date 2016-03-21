a6ee720dc91777b7618e5d28f9ef3d62d7a7309a
Todd Whiteman <todd.whiteman@joyent.com>
2016-03-21 10:50:13 -0700

    DOCKER-755: docker build: delay the 'Successfully built' message * allow sdc-docker to send this message when it's finished

--- bin/docker-build.js
+++ bin/docker-build.js
@@ -331,6 +331,7 @@ function buildFromContext(opts, callback) {
         containerRootDir: path.join('/zones', opts.uuid, 'root'),
         contextFilepath: opts.contextFilepath,
         dockerfile: opts.payload.dockerfile,
+        suppressSuccessMsg: true,  // Stop the 'Build successful: ' message.
         existingImages: opts.payload.allDockerImages,
         log: opts.log,
         nocache: opts.payload.nocache,
