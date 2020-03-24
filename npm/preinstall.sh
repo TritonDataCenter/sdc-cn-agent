aaf8ebb0e4ef3554820fb9be4b779acfe1ac9d17
Mike Gerdts <mike.gerdts@joyent.com>
2020-03-24 21:30:07 +0000

    preinstall needs to be linux aware

--- npm/preinstall.sh
+++ npm/preinstall.sh
@@ -6,7 +6,7 @@
 #
 
 #
-# Copyright (c) 2018, Joyent, Inc.
+# Copyright 2020 Joyent, Inc.
 #
 
 if [[ "${SDC_AGENT_SKIP_LIFECYCLE:-no}" = "yes" ]]; then
@@ -16,7 +16,26 @@ fi
 
 # Refuse to install on a platform less than the supported min-platform.
 MIN_PLATFORM=20151126T062538Z
-if [[ $(uname -v | cut -d '_' -f2) < $MIN_PLATFORM ]]; then
+case $(uname -s) in
+Linux)
+    # XXX this may need work to work in a container.
+    platform=$(source /etc/os-release; echo $TRITON_RELEASE)
+    ;;
+SunOS)
+    platform=$(uname -v | cut -d_ -f2)
+    ;;
+*)
+    echo "$0: $(uname -s) is not a supported operating system" 1>&2
+    exit 1
+    ;;
+esac
+
+if [[ -z $platform ]]; then
+    echo "$0: unable to determine the platform image" 1>&2
+    exit 1
+fi
+
+if [[ $platform < $MIN_PLATFORM ]]; then
     echo "FATAL: min-platform for this agent is $MIN_PLATFORM" >&2
     exit 2
 fi
